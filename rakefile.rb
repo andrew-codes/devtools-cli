0fd7064443a5c13a91cdb6291a71abd255b3f860
Andrew Smith <Andrew@WeAreSoftwareCraftsmen.com>
2014-02-11 12:12:16 -0500

    Configuring git for windows
    https://github.com/jamesandrewsmith/devtools/issues/22

--- rakefile.rb
+++ rakefile.rb
@@ -1,9 +1,13 @@
+require 'mustache'
+
 namespace :common do
 	desc "Enables array arguments"
 	task :parameters, :parameters do |t, args| 
 		ARGV.shift
 		ARGV.each do |arg|
     		task arg.to_sym do ; end
+    		task "windows:software:#{arg}".to_sym do ; end
+    		task "osx:software:#{arg}".to_sym do ; end
   		end
 	end
 end
@@ -12,12 +16,26 @@ namespace :windows do
 	desc "Installs required windows software"
 	task :install, [:software] => 'common:parameters' do
 		ARGV.each { |software|
-			exec "cinst #{software}"
+			sh "cinst #{software}"
 		}
 	end
 
-	desc "Configures windows software"
-	task :configure, [:software] => :install do
+	desc "Installs and configures windows software"
+	task :configure, [:software] => :install do |t, args|
+		ARGV.each { |software|
+			Rake::Task["windows:software:#{software}"].invoke
+		}
+	end
+
+	namespace :software do
+		desc "Configure git for windows"
+		task :git do
+			puts 'Configuring git for windows'
+			gitConfiguration = Hash.new
+			gitConfiguration[:alias] = ''
+			gitTemplate = File.read('global_-software-configuration/git/gitconfig.mustache')
+			puts Mustache.render(gitTemplate, gitConfiguration)
+		end
 	end
 end
 
@@ -27,7 +45,17 @@ namespace :osx do
 		
 	end
 
-	desc "Configures osx software"
-	task :configure, [:software] => :install do
+	desc "Installs and configures osx software"
+	task :configure, [:software] => :install do |t, args|
+		ARGV.each { |software|
+			Rake::Task["osx:software:#{software}"].invoke
+		}
+	end
+	
+	namespace :software do
+		desc "Configure git for osx"
+		task :git do
+			puts 'Configuring git for OSX'
+		end
 	end
 end
