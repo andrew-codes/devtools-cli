138f92cc622de96c72e1517306da208354b0877d
Andrew Smith <Andrew@WeAreSoftwareCraftsmen.com>
2014-02-13 12:31:55 -0500

    defects

--- lib/power_shell.rb
+++ lib/power_shell.rb
@@ -1,6 +1,6 @@
 class PowerShell
   def self.run(command)
-    system "powershell #{command} && exit"
+    system "powershell try{#{command}} catch[system.exception] {write-host $error[0]} finally{ exit }"
   end
 
   def self.run_cmd(command)
