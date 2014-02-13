6805eaf0e52abc8388a98379902f7d8c23b1b0ee
Andrew Smith <Andrew@WeAreSoftwareCraftsmen.com>
2014-02-13 14:40:16 -0500

    defects

--- lib/power_shell.rb
+++ lib/power_shell.rb
@@ -2,10 +2,10 @@ class PowerShell
   @@pids = []
 
   def self.run(command)
-    system "powershell.exe #{command}"
+    system "powershell.exe #{command} && exit"
   end
 
   def self.run_cmd(command)
-    self.run "cmd.exe #{command}"
+    self.run "cmd.exe #{command} && exit"
   end
 end
