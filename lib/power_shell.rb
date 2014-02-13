ebe82301ebf1edbd5feabfe40354e5662d59b81a
Andrew Smith <Andrew@WeAreSoftwareCraftsmen.com>
2014-02-13 12:47:02 -0500

    defects

--- lib/power_shell.rb
+++ lib/power_shell.rb
@@ -1,6 +1,7 @@
 class PowerShell
   def self.run(command)
-    system "powershell -Command { Try{ #{command} } Catch {write-host $error[0] } }"
+                command.gsub(/.*[']*.*/, "\\'")
+    system "powershell -Command 'Try{ #{command} } Catch {write-host $error[0] }'"
   end
 
   def self.run_cmd(command)
