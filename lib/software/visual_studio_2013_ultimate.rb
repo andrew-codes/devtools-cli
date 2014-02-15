67b3d3742efbb6c191545b7f9bc2f9a8dc7bec5e
Andrew Smith <Andrew@WeAreSoftwareCraftsmen.com>
2014-02-15 14:44:07 -0500

    Cannot configure VS; removed this functionality

--- lib/software/visual_studio_2013_ultimate.rb
+++ lib/software/visual_studio_2013_ultimate.rb
@@ -15,14 +15,7 @@ class VisualStudio2013Ultimate < Software
 
   def configure_for(platform)
     if platform == :windows
-      import_settings 'solarized-dark.vssettings '
+
     end
-  end
-
-  private
-  @visual_studio_exe = 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe'
-
-  def import_settings(settings_file)
-    @shell.run %Q|'#{@visual_studio_exe}' /Command Tools.ImportandExportSettings /import:'#{configatron.devtools}/settings/windows/visual-studio-2013/#{settings_file}'|, :windows
   end
 end
