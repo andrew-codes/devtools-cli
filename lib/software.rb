41d80ea34ad32fa526ac1ead091291c3bca25576
Andrew Smith <Andrew@WeAreSoftwareCraftsmen.com>
2014-02-15 10:13:15 -0500

    Changed way software installers are loaded

--- lib/software.rb
+++ lib/software.rb
@@ -1,5 +1,17 @@
+require 'software_installer'
+require 'utilities/shell'
+
 class Software
   def initialize(shell)
     @shell = shell
+  end
+
+  class << self
+    attr_reader :list
+  end
+  @list = []
+
+  def self.inherited(klass)
+    @list << SoftwareInstaller.new(klass.new(Shell.new))
   end
 end
--- lib/supported_software.rb
+++ lib/supported_software.rb
@@ -1,18 +1,7 @@
-require_relative 'software_installer'
-require_relative 'utilities/shell'
+require_relative 'software'
 
 class SupportedSoftware
   def self.get_all
-    shell = Shell.new
-    software = []
-    before = ObjectSpace.each_object(Class).to_a
-    Dir['software/*.rb'].each { |file|
-      require_relative file
-    }
-    after = ObjectSpace.each_object(Class).to_a
-    (after-before).map { |klass|
-      software.push SoftwareInstaller.new(klass.new(shell))
-    }
-    software
+    Software.list
   end
 end
