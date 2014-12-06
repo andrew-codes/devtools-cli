700c74efd2b4937df08dd1a10470c2a50406a048
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2014-12-06 13:10:56 -0500

    Installs atom in addition to configuring it

--- commands/install/atom.js
+++ commands/install/atom.js
@@ -3,73 +3,75 @@
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var _ = require('underscore');
-var shell = require('./../../lib/utils/Shell');
+var Shell = require('./../../lib/utils/Shell');
+var Log = require('./../../lib/utils/Log');
 
 var packages = [
-  'solarized-dark-ui',
-  'merge-conflicts',
-  'minimap',
-  'atom-beautify',
-  'refactor',
-  'js-refactor',
-  'coffee-refactor',
-  'grammar-selector',
-  'react',
-  'git-history',
-  'todo-show',
-  'es-navigation',
-  'coffee-navigation',
-  'atomic-rest',
-  'tabs-to-spaces',
-  'snippets',
-  'linter',
-  'linter-jshint',
-  'linter-jsxhint',
-  'linter-coffeelint',
-  'linter-less',
-  'linter-csslint',
-  'linter-spellcheck',
-  'linter-htmlhint',
-  'lang-csharp',
-  'autocomplete-plus-asyc',
-  'omnisharp-atom',
-  'package-generator',
-  'branch-status'
+	'solarized-dark-ui',
+	'merge-conflicts',
+	'minimap',
+	'atom-beautify',
+	'refactor',
+	'js-refactor',
+	'coffee-refactor',
+	'grammar-selector',
+	'react',
+	'git-history',
+	'todo-show',
+	'es-navigation',
+	'coffee-navigation',
+	'atomic-rest',
+	'tabs-to-spaces',
+	'snippets',
+	'linter',
+	'linter-jshint',
+	'linter-jsxhint',
+	'linter-coffeelint',
+	'linter-less',
+	'linter-csslint',
+	'linter-spellcheck',
+	'linter-htmlhint',
+	'lang-csharp',
+	'autocomplete-plus-asyc',
+	'omnisharp-atom',
+	'package-generator',
+	'branch-status'
 ];
 
 var atom = {
-  install: install
+	install: install
 };
 module.exports = atom;
 
-var envOptions;
 function install(options) {
-  envOptions = options;
-  if (!isInstalled()) {
-    return options;
-  }
-  return Promise.all(installPackages())
-  .catch(function(e){
-    console.log(e);
-  })
-  .then(_.constant(options));
+	return installAtom(options)
+		.then(function () {
+			return Promise.settle(installPackages())
+				.then(function (results) {
+					_.chain(results)
+						.filter(function (r) {
+							return r.isRejected();
+						})
+						.each(function (r) {
+							Log.error(r.reason());
+						})
+				});
+		})
+		.then(_.constant(options));
 }
 
-function isInstalled() {
-  if (envOptions.isOsx()){
-    return fs.existsSync('/Applications/Atom.app');
-  }
-  if (envOptions.isWindows()){
-    var isInstalledOnWindows = false;
-    fs.readdirSync('C:\\ProgramData\\chocolatey\\lib', function(items){
-      isInstalledOnWindows = isInstalledOnWindows || items.toLowerCase().indexOf('atom') === 0;
-    });
-    return isInstalledOnWindows;
-  }
+function installAtom(options) {
+	if (options.isOsx()) {
+		return Shell.run('brew cask install atom');
+	} else if (options.isWindows()) {
+		return Shell.run('choco install Atom');
+	} else {
+		throw 'Platform not supported for Atom: ' + options.platform;
+	}
 }
 
 function installPackages() {
-  return _.map(packages, function(pkg){
-    return shell.run('apm install ' + pkg);
-  });
+	return _.map(packages, function (pkg) {
+		return Shell.run('apm install ' + pkg);
+	});
 }
