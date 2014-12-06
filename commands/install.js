7815e9f332b81405c1ff2dc82c4f6ce741984de1
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2014-12-06 13:11:38 -0500

    Installs choco (windows) or homebrew/cask (OSX) for support of installing programs in addition to configuring them

--- commands/install.js
+++ commands/install.js
@@ -10,85 +10,114 @@ var _ = require('underscore');
 var path = require('path');
 var Symlink = require('./../lib/utils/Symlink');
 var Config = require('./../lib/utils/Config');
+var Log = require('./../lib/utils/Log');
+var Shell = require('./../lib/utils/Shell');
 
 var install = {
-    getCompletion: getCompletion,
-    setup: setup
+	getCompletion: getCompletion,
+	setup: setup
 };
 module.exports = install;
 
 function getCompletion() {
-    return function (words, prev, cur) {
-        complete.output(cur, ['--user']);
-    };
+	return function (words, prev, cur) {
+		complete.output(cur, ['--user']);
+	};
 }
 
 function setup(program) {
-    program
-        .command('install')
-        .description('Install to target platform')
-        .action(action)
-        .option('-u, --user [user]', 'User name');
+	program
+		.command('install')
+		.description('Install to target platform')
+		.action(action)
+		.option('-u, --user [user]', 'User name');
 }
 
 var dist = './dist';
+
 function action() {
-    var options = getOptions(this.user);
-    var installStream = FileUtils.rmdir(dist)
-        .then(function () {
-            return FileUtils.mkdir(options.targetDir);
-        }).
-        then(function () {
-            return options;
-        });
-    _.each(Object.keys(installSteps), function (key) {
-        installStream = installStream.then(installSteps[key].install);
-    });
-    installStream.then(finalize)
-        .catch(function (e) {
-            console.log(e);
-        });
+	var options = getOptions(this.user);
+	FileUtils.rmdir(dist)
+		.then(function () {
+			return FileUtils.mkdir(options.targetDir);
+		})
+		.then(function () {
+			return initializeEnvironment(options);
+		})
+		.then(function () {
+			var installations = _.chain(Object.keys(installSteps))
+				.filter(function (key) {
+					return _.contains(options.includeApplications, key);
+				})
+				.map(function (key) {
+					console.log('Starting installation: ' + key)
+					return installSteps[key].install(options)
+						.tap(function () {
+							console.log('Finished installation: ' + key);
+						});
+				}).value();
+			Promise.all(installations)
+				.then(_.constant(options))
+				.then(finalize)
+				.catch(Log.error);
+		});
 }
 
 function finalize(options) {
-    return finalizeItems(options.targetDir, options);
+	console.log('Finalizing...');
+	return finalizeItems(options.targetDir, options);
 }
 
 function getOptions(user) {
-    return Config.applyForUser(user);
+	return Config.applyForUser(user);
 }
 
 function finalizeItems(directory, options) {
-    return fs.readdirAsync(directory)
-        .then(function (dirItems) {
-            var items = _.map(dirItems, function (item) {
-                var itemPath = path.join(directory, item);
-                return {
-                    path: itemPath,
-                    Name: item,
-                    isDirectory: fs.lstatSync(itemPath).isDirectory()
-                };
-            });
-            var promises = _.chain(items)
-                .filter(function (item) {
-                    return !item.isDirectory;
-                })
-                .map(function (file) {
-                    return finalizeItem(file, options);
-                }).value();
+	return fs.readdirAsync(directory)
+		.then(function (dirItems) {
+			var items = _.map(dirItems, function (item) {
+				var itemPath = path.join(directory, item);
+				return {
+					path: itemPath,
+					Name: item,
+					isDirectory: fs.lstatSync(itemPath).isDirectory()
+				};
+			});
+			var promises = _.chain(items)
+				.filter(function (item) {
+					return !item.isDirectory;
+				})
+				.map(function (file) {
+					return finalizeItem(file, options);
+				}).value();
 
-            promises.concat(_.chain(items)
-                .filter(function (item) {
-                    return item.isDirectory;
-                })
-                .map(function (dir) {
-                    return finalizeItems(dir.path, options);
-                }).value());
+			promises.concat(_.chain(items)
+				.filter(function (item) {
+					return item.isDirectory;
+				})
+				.map(function (dir) {
+					return finalizeItems(dir.path, options);
+				}).value());
 
-            return _.flatten(promises);
-        });
+			return _.flatten(promises);
+		});
 }
 
 function finalizeItem(file, options) {
-    return Symlink.mklink(options.platform, file.path, options.dest(file.path));
+	return Symlink.mklink(options.platform, file.path, options.dest(file.path));
+}
+
+
+function initializeEnvironment(options) {
+	if (options.isOsx()) {
+		return Shell.run('ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+			.catch(Log.error)
+			.then(function () {
+				return Shell.run('brew install cask')
+					.catch(Log.error);
+			});
+	} else if (options.isWindows()) {
+		return Shell.run('@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin\\')
+			.catch(Log.error);
+	}
 }
