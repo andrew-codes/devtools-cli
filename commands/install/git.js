da3efebe0a08d7f03f845ab6af1567ba3a457a2f
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2014-12-06 13:17:45 -0500

    Support installing git

--- commands/install/git.js
+++ commands/install/git.js
@@ -3,21 +3,36 @@
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var Template = require('./../../lib/utils/Template');
+var Shell = require('./../../lib/utils/Shell');
+var Log = require('./../../lib/utils/Log');
 
 var git = {
-    install: install
+	install: install
 };
 module.exports = git;
 
 function install(options) {
-    return Template.combineInTemplate('settings/git/.gitconfig.mustache', '', options)
-        .then(function () {
-            return fs.readFileAsync('settings/git/.gitignore', 'utf-8')
-                .then(function (data) {
-                    return fs.writeFileAsync(options.targetDir + '/.gitignore', data);
-                })
-                .then(function () {
-                    return options;
-                });
-        });
+	return installGit(options).
+	then(function () {
+			return Template.combineInTemplate('settings/git/.gitconfig.mustache', '', options)
+				.then(function () {
+					return fs.readFileAsync('settings/git/.gitignore', 'utf-8')
+						.then(function (data) {
+							return fs.writeFileAsync(options.targetDir + '/.gitignore', data);
+						})
+				});
+		})
+		.catch(Log.error)
+		.then(function () {
+			return options;
+		});
+}
+
+function installGit(options) {
+	if (options.isOsx()) {
+		return Shell.run('brew install git');
+	} else if (options.isWindows()) {
+		return Shell.run('choco install git');
+	}
+	throw 'Platform not supported for Git: ' + options.platform;
 }
